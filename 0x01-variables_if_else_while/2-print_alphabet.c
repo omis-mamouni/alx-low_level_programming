f5bf457ba477a7dfb93f57d71a9ffadcd3ca9c43
omis-mamouni <omismamouni@gmail.com>
2023-03-17 17:14:45 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,20 +6,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void){
-
-        int n, co;
-
-        co = 0;
-
-        while (co < 10)
-        {
-                for (n = 'a'; n <= 'z'; n++)
-                {
-                        putchar(n);
-                }
-                co++;
-                putchar('\n');
+int main(void)
+{
+	int n, co;
+	co = 0;
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
 	}
-return (0);
+	putchar('\n');
+	return (0);
 }
