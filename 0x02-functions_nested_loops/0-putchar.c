3c799fabb96c16fb0830991c9dff5d75dbe4d65d
omis-mamouni <omismamouni@gmail.com>
2023-03-21 15:47:19 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,11 +11,11 @@ int main(void)
 	putchar('_');
 	putchar('p');
 	putchar('u');
-    	putchar('t');
-    	putchar('c');
-    	putchar('h');
-    	putchar('a');
-    	putchar('r');
-    	putchar('\n');
-    	return (0);
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
 }
