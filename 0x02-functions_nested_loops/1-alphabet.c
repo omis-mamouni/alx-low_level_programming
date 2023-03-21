6417243c06879677cc0cab57723c7171f95f5f7d
omis-mamouni <omismamouni@gmail.com>
2023-03-21 16:23:36 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+        char i;
+        for (i ='a' ; i <= 'z' ; i++)
+                putchar (i);
+        putchar('\n');
+}
+int main(void)
+{
+        print_alphabet();
+        return (0);
+}
